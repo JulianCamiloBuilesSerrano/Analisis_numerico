2dd64f707e5443d26b0330f81b123388c2fa5850
root <root@Julian-PC.localdomain>
2020-09-13 15:32:12 -0500

    Punto 2 mejorado

--- Retos/aproximacionPolinomica.R
+++ Retos/aproximacionPolinomica.R
@@ -12,32 +12,41 @@ library(pracma)
 library(MASS)
 library(signal)
 library(Rmpfr)
-bits = 300
+bits = 128
 # -------------------------------------------------------------------------------------
-#   2.Parte 1: Evaluar en Taylor
+#     Evaluar en Taylor
 #     Para este punto se hizo uso de la funcion Taylor en la libreria Pracma
 #     para asi poder obtener el polinomio de taylor y luego esta la funcion 
-#     evaluarEnTaylor() la cual evalua el polinomio en el punto dado el cual
-#     es obtenido del intervalo. Se evaluo el polinomio en 10 puntos del
-#     
+#     evaluarla dentro de los intervalos 
+#          
 # -------------------------------------------------------------------------------------
-#Polinomio de Taylor en Sen(X)
+#Polinomio de Taylor
 f = function(x)
 {
   return (exp(sin(x)-sin(x)^2))
   #return(sin(x))
 }
 
-polinomioCalculado = mpfr(taylor(f,0,50),bits)
+polinomioCalculado = mpfr(taylor(f,0,3),bits)
 
-help("taylor")
-print(polinomioCalculado)
-inicio =  mpfr(-2^(-8),bits)
+inicio = mpfr( -2^(-8),bits)
 final = mpfr(2^(-8),bits)
 
-s= seq( inicio,final,0.0009 )
-print(s)
-for i in s{
-  horner(p = polinomioCalculado,x = c(2^(-8)))
-}
-print(f(2^(-8)))
+s = seq( inicio,final,0.0009 )
+error = c()
+for (i in range(1,length(s))){
+  print("evaluando")
+  print(s[i])
+  r =horner(p = polinomioCalculado, s[i])
+  cat("el valor con taylor es\t")
+  print(r$y)
+  x = mpfr(f(s[i]),bits)
+  cat("el valor esperado es ")
+  print(x)
+  print(abs(x-r$y))
+  error <- c(error,abs(x-r$y))
+} 
+print(error)
+polyApprox(f=f, a = inicio,b = final,n =3 )
+
+
