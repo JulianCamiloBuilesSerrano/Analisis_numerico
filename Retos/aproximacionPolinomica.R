156e05d37b055b45a336b09ccdfa80895a31c7c9
Julian Camilo Builes Serrano <juliancho147@users.noreply.github.com>
2020-09-13 21:11:36 -0500

    sin el doc

--- Retos/aproximacionPolinomica.R
+++ Retos/aproximacionPolinomica.R
@@ -23,22 +23,35 @@ polinomioCalculado = mpfr(taylor(f,0,3),bits)
 
 inicio = mpfr( -2^(-8),bits)
 final = mpfr(2^(-8),bits)
-s = seq( inicio,final,0.00009 )
+s = seq( inicio,final,0.0005 )
 error = c()
 for (i in seq(length(s))){
-  print("evaluando")
-  print(i)
+cat("Numero Evaluado en la funcion y el polinomio ")
+  print(s[i])
   r =horner(p = polinomioCalculado, s[i])
   cat("el valor con taylor es\t")
   print(r$y)
+  plot(asNumeric(r$y),s[i],type='p',col = 'red')
   x = mpfr(f(s[i]),bits)
   cat("el valor esperado es ")
   print(x)
-  print('+++++++++++++++++++++++++++++++++')
+  print('------------------------------------')
   error <- c(error,abs(x-r$y))
 } 
-print(error)
-
-polyApprox(f=f, a = inicio,b = final,n =3 )
-
-
+# Evaluar Ramez con la fuuncion que usa los numeros de cehvicev y da una aproximacion 
+# al poliomio
+ramez = polyApprox(f = f, a = -2^(-8),b = 2^(-8),n = 3)
+error2 <- c()
+for(i in seq(length(s))){
+  cat("Numero Evaluado en la funcion y el polinomio ")
+  print(s[i])
+  r =horner(p = ramez$p, s[i])
+  cat("el valor con taylor es\t")
+  print(r$y)
+  plot(asNumeric(r$y),s[i],type='p',col = 'red')
+  x = mpfr(f(s[i]),bits)
+  cat("el valor esperado es ")
+  print(x)
+  print('------------------------------------')
+  error2 <- c(error,abs(x-r$y))
+}
